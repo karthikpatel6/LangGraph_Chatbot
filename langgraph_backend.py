bfd789c488ec731fd3c3c0181dd9a93f6f6b39b6
karthikpatel6 <karthikkpatel06@gmail.com>
2026-05-28 22:22:15 +0530

    DataBase is creates

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -2,8 +2,9 @@ from langgraph.graph import StateGraph,START,END
 from typing import TypedDict,Annotated
 from langchain_core.messages import BaseMessage
 from langchain_mistralai import ChatMistralAI
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
+import sqlite3
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -17,7 +18,9 @@ def chat_node(state : ChatState):
      response = model.invoke(messages)
      return {"messages" : [response]}
 
-checkpointer = InMemorySaver()
+conn = sqlite3.connect(database='chatbot.db',check_same_thread=False)
+
+checkpointer = SqliteSaver(conn=conn)
 
 graph = StateGraph(ChatState)
 graph.add_node("chat_node",chat_node)
@@ -26,3 +29,10 @@ graph.add_edge(START,"chat_node")
 graph.add_edge("chat_node",END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+def retrieve_all_threads():
+     all_threads = set()
+     for checkpoint in checkpointer.list(None):
+          all_threads.add(checkpointer.config['configurable']['thread_id'])
+
+     return list(all_threads)     
